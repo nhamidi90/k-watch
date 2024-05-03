8c267fff533165816a93c9db66763f7914d415a8
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-05-03 13:17:58 +0000

    Initialize dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     let navbar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(navbar);
+
+    let dropdown = document.querySelectorAll('select');
+    M.FormSelect.init(dropdown);
 });
