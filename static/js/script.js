02e082d82cd28636812f40dd23f0e5cce96df60b
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-05-05 08:02:24 +0000

    Add star rating

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let dropdown = document.querySelectorAll('select');
     M.FormSelect.init(dropdown);
+
+    let star = document.querySelectorAll('input');
+    let showValue = document.querySelector('#rating-value');
+
+    for (let i = 0; i < star.length; i++) {
+	    star[i].addEventListener('click', function() {
+		    i = this.value;
+
+		showValue.innerHTML = i + " out of 10";
+	});
+}
 });
