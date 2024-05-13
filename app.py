42863a0807f1040f1f92ef6715ca503fc76cdbef
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-05-13 20:16:49 +0000

    Remove unused code

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ def add_upcoming():
 @app.route("/add_new_drama", methods=["GET", "POST"])
 def add_new_drama():
     if request.method == "POST":
-        status = list(mongo.db.status.find())
         new_drama = {
         "title": request.form.get("title"),
         "image": request.form.get("img_url"),
@@ -72,7 +71,7 @@ def add_new_drama():
         }
         mongo.db.shows.insert_one(new_drama)
         flash("Your drama was added successfully")
-        return redirect(url_for("index", status=status))
+        return redirect(url_for("index"))
 
 
 @app.route("/delete_upcoming/<coming_id>")
