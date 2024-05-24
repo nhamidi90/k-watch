bbb0684bbf059435cd0a1547060fb5d55a735717
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-05-24 11:03:56 +0000

    Add validation for dropdown in modals

--- static/js/script.js
+++ static/js/script.js
@@ -116,6 +116,37 @@ document.addEventListener('DOMContentLoaded', function () {
         let classInvalid = "border-bottom: 1px solid #f44336; box-shadow: 0 1px 0 0 #f44336;";
         let selectValidate = document.querySelector("select.validate");
         let selectWrapperInput = document.querySelector(".select-wrapper input.select-dropdown");
+        let selectWrapperInputs = document.getElementsByClassName('dropdown-trigger');
+        for (let i=0; i < selectWrapperInputs.length; i++) {
+            selectWrapperInputs[i].addEventListener("focusin", (e) => {
+                e.target.parentNode.addEventListener("change", () => {
+                    ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+                    for (let i = 0; i < ulSelectOptions.length; i++) {
+                        if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                            e.target.style.cssText = classValid;
+                        }
+                    }
+                });
+            });
+            selectWrapperInputs[i].addEventListener("click", (e) => {
+                ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+                for (let i = 0; i < ulSelectOptions.length; i++) {
+                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
+                        e.target.style.cssText = classValid;
+                    } else {
+                        e.target.addEventListener("focusout", () => {
+                            if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
+                                console.log('this field is required')
+                                if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
+                                    e.target.style.cssText = classInvalid;
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
         if (selectValidate.hasAttribute("required")) {
             selectValidate.style.cssText = "display: block; height: 0; padding: 0; width: 0; position: absolute;";
         }
