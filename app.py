b9e3f51e2fd2106f61d6312baeec4242e579a7e8
Nabilah Hamidi <nabilahhamidi@hotmail.com>
2024-05-30 10:27:16 +0000

    Change debug to false

--- app.py
+++ app.py
@@ -325,4 +325,4 @@ def delete_drama(show_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
